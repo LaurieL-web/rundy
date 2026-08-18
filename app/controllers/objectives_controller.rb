3566a321c7bca811e6dd53565386e8c35f427fff
Laurie Lescos <laurie.oo@hotmail.fr>
2026-08-18 13:00:00 +0200

    Mise à jour branche master

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,2 +1,22 @@
 class ObjectivesController < ApplicationController
+
+  def new
+    @objective = Objective.new
+  end
+  def create
+    @objective = Objective.new(objective_params)
+    @user = current_user
+    @objective.user = @user
+    if @objective.save
+      redirect_to sessions_path(@objective)
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def objective_params
+    params.require(:objective).permit(:distance, :target_time, :prepa_duration, :frequency)
+  end
 end
